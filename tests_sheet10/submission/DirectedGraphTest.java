932724781e6e60e2092cf688e2d00f10990b2e6d
Pherkel <philippmerkel@outlook.com>
2022-07-07 23:25:50 +0200

    finished sheet10 (could add more tests)

--- tests_sheet10/submission/DirectedGraphTest.java
+++ tests_sheet10/submission/DirectedGraphTest.java
@@ -1,5 +1,6 @@
 package submission;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Array;
@@ -19,6 +20,7 @@ class DirectedGraphTest {
     }
 
     @Test
+    @DisplayName("shortestPathBetween - sheet example")
     void shortestPathBetween() {
         DirectedGraph graph = new DirectedGraph(4);
         graph.connect(0, 2, 9);
@@ -30,5 +32,32 @@ class DirectedGraphTest {
         ArrayList<Integer> expectedArray = new ArrayList<>(Arrays.asList(0, 3, 1, 2));
 
         assertEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    @DisplayName("costOfPath - sheet example")
+    void costOfPath_sheet_example() {
+        DirectedGraph graph = new DirectedGraph(4);
+        graph.connect(0, 2, 9);
+        graph.connect(0, 3, 1);
+        graph.connect(3, 1, 2);
+        graph.connect(1, 2, 3);
+
+        ArrayList<Integer> testArray = new ArrayList<>(Arrays.asList(0, 3, 1, 2));
+        assertEquals(6.0, graph.costOfPath(testArray));
+    }
+
+    @Test
+    @DisplayName("costOfPath - path not connected")
+    void costOfPath() {
+        DirectedGraph graph = new DirectedGraph(4);
+        graph.connect(0, 2, 9);
+        graph.connect(0, 3, 1);
+        graph.connect(3, 1, 2);
+        graph.connect(1, 2, 3);
+
+        ArrayList<Integer> testArray = new ArrayList<>(Arrays.asList(0, 1, 2));
+
+        assertEquals(Double.POSITIVE_INFINITY, graph.costOfPath(testArray));
     }
 }
