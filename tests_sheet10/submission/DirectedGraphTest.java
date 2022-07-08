9ea04bb772b1d1cc74395b1979f76e367e6684f2
Pherkel <philippmerkel@outlook.com>
2022-07-08 11:16:31 +0200

    added more complex test graph

--- tests_sheet10/submission/DirectedGraphTest.java
+++ tests_sheet10/submission/DirectedGraphTest.java
@@ -12,16 +12,56 @@ import static org.junit.jupiter.api.Assertions.*;
 class DirectedGraphTest {
 
     @Test
-    void adjacentNodes() {
+    @DisplayName("adjacentNodes - sheet example")
+    void adjacentNodes_sheet() {
+        DirectedGraph graph = new DirectedGraph(4);
+        graph.connect(0, 2, 0);
+        graph.connect(0, 3, 0);
+        graph.connect(1, 3, 0);
+
+        assertEquals(Arrays.asList(2, 3), graph.adjacentNodes(0));
+        assertEquals(Arrays.asList(3), graph.adjacentNodes(1));
+        assertEquals(Arrays.asList(), graph.adjacentNodes(2));
     }
 
     @Test
-    void pathExistsBetween() {
+    @DisplayName("pathExistsBetween - sheet example")
+    void pathExistsBetween_sheet() {
+        DirectedGraph graph = new DirectedGraph(4);
+        graph.connect(0, 2, 0);
+        graph.connect(0, 3, 0);
+        graph.connect(3, 1, 0);
+
+        assertFalse(graph.pathExistsBetween(2, 0));
+        assertTrue(graph.pathExistsBetween(0, 1));
+    }
+
+    @Test
+    @DisplayName("pathExistsBetween - complex example 1")
+    void pathExistsBetween_complex_1() {
+        DirectedGraph graph = new DirectedGraph(10);
+        graph.connect(4, 0, 7);
+        graph.connect(0, 6, 7);
+        graph.connect(6, 0, 7);
+        graph.connect(6, 4, 1);
+        graph.connect(6, 9, 3);
+        graph.connect(9, 7, 42);
+        graph.connect(9, 2, 3);
+        graph.connect(2, 6, 3);
+        graph.connect(2, 5, 3);
+        graph.connect(5, 1, 3);
+        graph.connect(1, 8, 3);
+        graph.connect(8, 5, 3);
+        graph.connect(5, 3, 3);
+        graph.connect(3, 8, 3);
+
+        assertFalse(graph.pathExistsBetween(7, 3));
+        assertTrue(graph.pathExistsBetween(6, 1));
     }
 
     @Test
     @DisplayName("shortestPathBetween - sheet example")
-    void shortestPathBetween() {
+    void shortestPathBetween_sheet_example() {
         DirectedGraph graph = new DirectedGraph(4);
         graph.connect(0, 2, 9);
         graph.connect(0, 3, 1);
@@ -32,6 +72,38 @@ class DirectedGraphTest {
         ArrayList<Integer> expectedArray = new ArrayList<>(Arrays.asList(0, 3, 1, 2));
 
         assertEquals(expectedArray, actualArray);
+    }
+
+    @Test
+    @DisplayName("shortestPathBetween - complex example")
+    void shortestPathBetween_complex() {
+        DirectedGraph graph = new DirectedGraph(10);
+        graph.connect(4, 0, 7);
+        graph.connect(0, 6, 7);
+        graph.connect(6, 0, 7);
+        graph.connect(6, 4, 1);
+        graph.connect(6, 9, 3);
+        graph.connect(9, 7, 42);
+        graph.connect(9, 2, 3);
+        graph.connect(2, 6, 3);
+        graph.connect(2, 5, 3);
+        graph.connect(5, 1, 3);
+        graph.connect(1, 8, 3);
+        graph.connect(8, 5, 3);
+        graph.connect(5, 3, 3);
+        graph.connect(3, 8, 3);
+
+        ArrayList<Integer> actualPath_4_1 = graph.shortestPathBetween(4, 1);
+        ArrayList<Integer> expectedPath_4_1 = new ArrayList<>(Arrays.asList(4, 0, 6, 9, 2, 5, 1));
+
+        assertEquals(expectedPath_4_1, actualPath_4_1);
+        assertEquals(26.0, graph.costOfPath(actualPath_4_1));
+
+        ArrayList<Integer> actualPath_9_8 = graph.shortestPathBetween(9, 8);
+        ArrayList<Integer> expectedPath_9_8 = new ArrayList<>(Arrays.asList(9, 2, 5, 1, 8));
+
+        assertEquals(expectedPath_9_8, actualPath_9_8);
+        assertEquals(12.0, graph.costOfPath(actualPath_9_8));
     }
 
     @Test
